30236b61905e4f3415be43d1c313189dfa08879b
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-04-11 20:01:35 +0100

    Add api request into LineList

--- src/components/LineList.js
+++ src/components/LineList.js
@@ -1,18 +1,59 @@
-import React, { useState } from "react";
-import * as api from "../api"
+import React, { useState, useEffect } from "react";
+import * as api from "../api";
 
 export default function LineList() {
   const [loading, setLoading] = useState(true);
+  // const [updateRequired, setUpdateRequired] = useState(true);
+  const [lines, setLines] = useState();
 
-  setTimeout(() => {setLoading(false)}, 5000)
-  api.getLineStatuses().then(() => {
-    console.log("api request made")
-  })
+  useEffect(() => {
+    console.log("sent request");
+    async function fetchData() {
+      try {
+        const response = await api.getLineStatuses();
+        console.log("response received", response);
+        setLines(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
 
+  useEffect(() => {
+    setLoading(false);
+  }, [lines]);
+
+  console.log(loading);
 
   if (loading) {
     return <h2>Loading...</h2>;
+  } else if (lines) {
+    return (
+      <div className="lineList">
+        <table>
+          <thead>
+            <tr>
+              <th>Line</th>
+              <th>Status</th>
+              <th>More Info</th>
+            </tr>
+          </thead>
+          <tbody>
+            {lines.map((line) => {
+              return (
+                <tr>
+                  <td>{line.name}</td>
+                  <td>Status goes here</td>
+                  <td>Link</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
   } else {
-    return <div className="lineList"></div>;
+    return <p>Shit went wrong yo</p>;
   }
 }
