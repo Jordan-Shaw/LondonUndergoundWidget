1290f921758736dba1b83cfb277e1d828735d8be
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-04-11 17:30:01 +0100

    Add getLineStatuses api req

---
+++ src/api.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+const baseURL = "https://api.tfl.gov.uk/";
+
+export const getLineStatuses = () => {
+  return (
+    axios.get(`${baseURL}line/mode/tube/status?app_key=`,
+    {params: { app_key: "2c620fbd67b0fe7202e99b15a97419d6", app_id: "cdf2be4d" }})
+  ).then(res => {
+    console.log(res)
+    return;
+  }).catch(err => {
+    console.log(err)
+  });
+};
