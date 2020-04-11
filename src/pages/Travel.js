94f193f352312826402a795483743e90ee0f1f81
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-04-11 17:05:53 +0100

    Move Travel and Home into new pages directory

--- src/pages/Travel.js
+++ src/pages/Travel.js
@@ -1,12 +1,17 @@
 import React from "react";
 import logo from "../logo.svg";
+import LineList from "../components/LineList";
 
 export default function Travel() {
   return (
     <div className="travelPage">
       <div className="travelHeader">
         <div className="headerBackground"></div>
-        <img src={logo} alt="Widget Logo" className="logo"/>
+        <img src={logo} alt="Widget Logo" className="logo" />
+      </div>
+      <div className="contentContainer">
+        <p>Last updated:</p>
+        <LineList/>
       </div>
     </div>
   );
