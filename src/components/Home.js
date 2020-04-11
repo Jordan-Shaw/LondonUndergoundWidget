6e95afc8137ccda135ff52cb61c38673a1a484b0
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-04-11 16:34:00 +0100

    Add basic structure of homepage

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "../logo.svg";
+import { useNavigate } from "@reach/router";
+
+export default function Home() {
+  const navigate = useNavigate();
+  return (
+    <div className="homePage">
+      <div className="homeBackground"></div>
+
+      <div className="homeContentContainer">
+        <img src={logo} className="logo" alt="Widget Logo" />
+        <h2 className="homeSubheading">
+          Helping you get where you need to go...
+        </h2>
+        <button
+          className="enterButton"
+          onClick={() => {
+            navigate("/travel");
+          }}
+        >
+          ENTER
+        </button>
+      </div>
+    </div>
+  );
+}
