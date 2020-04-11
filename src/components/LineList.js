35cabdbea1e36dd25f2e14dbc5f448c496c22bc1
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-04-11 17:30:18 +0100

    Add basics of LineList

--- src/components/LineList.js
+++ src/components/LineList.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import * as api from "../api"
 
 export default function LineList() {
-  return (
-    <div className="lineList">
-      
-    </div>
-  )
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {setLoading(false)}, 5000)
+  api.getLineStatuses().then(() => {
+    console.log("api request made")
+  })
+
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  } else {
+    return <div className="lineList"></div>;
+  }
 }
