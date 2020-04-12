f4b298f8a8906d84149ca3d1a72d50e5295bc97c
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-04-12 19:34:11 +0100

    Refactor of LineCard

--- src/components/LineCard.js
+++ src/components/LineCard.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Link } from "@reach/router";
 const { checkLineStatuses } = require("../utils.js");
 
 export default function LineCard(props) {
   const { line } = props;
+  const {name, lineStatuses, id} = line
   const lineStatusIndex = checkLineStatuses(line);
 
   return (
     <tr>
-      <td>{line.name}</td>
-      <td>{line.lineStatuses[lineStatusIndex].statusSeverityDescription}</td>
-      <td>Link</td>
+      <td>{name}</td>
+      <td>{lineStatuses[lineStatusIndex].statusSeverityDescription}</td>
+      <td>
+        <Link to={`${id}`}>Click here</Link>
+      </td>
     </tr>
   );
 }
