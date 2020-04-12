c4965ad1ce2e1ccf0306b5eb34c090da98a577db
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-04-12 16:07:47 +0100

    Add specific lineStatus information to table via LineCards

--- src/components/LineCard.js
+++ src/components/LineCard.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
+const { checkLineStatuses } = require("../utils.js");
 
 export default function LineCard(props) {
   const { line } = props;
+  const lineStatusIndex = useRef(checkLineStatuses(line));
+  console.log("lineStatusIndex.current", lineStatusIndex.current);
+
   console.log(line);
   return (
     <tr>
       <td>{line.name}</td>
-      <td>Status goes here</td>
+      <td>
+        {line.lineStatuses[lineStatusIndex.current].statusSeverityDescription}
+      </td>
       <td>Link</td>
     </tr>
   );
