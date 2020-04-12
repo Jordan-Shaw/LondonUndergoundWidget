91d07d889b6f525fd83ddef252831ef9cb5046ed
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-04-12 19:35:27 +0100

    Add single status request

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
-const baseURL = "https://api.tfl.gov.uk/";
+const baseURL = "https://api.tfl.gov.uk";
 
 export const getLineStatuses = () => {
   return axios
-    .get(`${baseURL}line/mode/tube/status`, {
+    .get(`${baseURL}/line/mode/tube/status`, {
       params: {
         app_key: "2c620fbd67b0fe7202e99b15a97419d6",
         app_id: "cdf2be4d",
@@ -16,3 +16,17 @@ export const getLineStatuses = () => {
       console.log(err);
     });
 };
+
+export const getSingleLineStatus = (line) => {
+  return axios.get(`${baseURL}/Line/${line}/Status`, {
+    params: {
+      detail: false,
+      app_key: "2c620fbd67b0fe7202e99b15a97419d6",
+      app_id: "cdf2be4d"
+    },
+  }).then((res) => {
+    return res;
+  }).catch((err) => {
+    console.log(err)
+  });
+}
