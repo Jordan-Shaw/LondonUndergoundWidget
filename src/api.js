2501e8f842078ff559b9104d1a9420c12713b16f
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-04-12 12:45:13 +0100

    Fix get request string

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const baseURL = "https://api.tfl.gov.uk/";
 
 export const getLineStatuses = () => {
   return axios
-    .get(`${baseURL}line/mode/tube/status?app_key=`, {
+    .get(`${baseURL}line/mode/tube/status`, {
       params: {
         app_key: "2c620fbd67b0fe7202e99b15a97419d6",
         app_id: "cdf2be4d",
