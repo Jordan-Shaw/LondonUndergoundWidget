dd54808812bdffeeeb80d2c3f0cedcd0d9521d5c
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-04-12 16:08:15 +0100

    Simplify LineCard

--- src/components/LineCard.js
+++ src/components/LineCard.js
@@ -1,18 +1,14 @@
-import React, { useRef } from "react";
+import React from "react";
 const { checkLineStatuses } = require("../utils.js");
 
 export default function LineCard(props) {
   const { line } = props;
-  const lineStatusIndex = useRef(checkLineStatuses(line));
-  console.log("lineStatusIndex.current", lineStatusIndex.current);
+  const lineStatusIndex = checkLineStatuses(line);
 
-  console.log(line);
   return (
     <tr>
       <td>{line.name}</td>
-      <td>
-        {line.lineStatuses[lineStatusIndex.current].statusSeverityDescription}
-      </td>
+      <td>{line.lineStatuses[lineStatusIndex].statusSeverityDescription}</td>
       <td>Link</td>
     </tr>
   );
